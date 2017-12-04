9082f900418e84306e6096c56d843fae1f726ce6
lei <1004552348@qq.com>
2017-12-04 17:54:45 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/administrator/coolweather/db/Province.java
@@ -0,0 +1,43 @@
+package com.example.administrator.coolweather.db;
+
+/**
+ * Created by Administrator on 2017/12/4.
+ */
+
+
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
+
